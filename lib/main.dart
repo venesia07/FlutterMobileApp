1e2577fb1229cf5bee5c5645b30a9f3ff1f49485
Akash Bajpai <aakashbajpai2003@gmail.com>
2026-07-18 14:51:18 +0530

    n/a

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'screens/home_screen.dart';
+import 'screens/program_list_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Excelerate App',
-      home: HomeScreen(),
+      home: ProgramListScreen(),
     );
   }
 }

f13fada51f2fa589c53dcabe291c42a435975d01
Akash Bajpai <aakashbajpai2003@gmail.com>
2026-07-18 15:45:45 +0530

    n/a

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -341,7 +341,7 @@ class _HomeScreenState extends State<HomeScreen> {
           decoration: BoxDecoration(
             color: Colors.white,
             borderRadius: BorderRadius.circular(12),
-            boxShadow: [BoxShadow(color: Colors.black.withOpacity(0.05), blurRadius: 6, offset: const Offset(0, 2))],
+            boxShadow: [BoxShadow(color: Colors.black.withValues(alpha: 0.05), blurRadius: 6, offset: const Offset(0, 2))],
           ),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,

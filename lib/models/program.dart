4e0d577d8647108571591200a2589db582dbba14
VenesiaSwartz <vswartz0605@gmail.com>
2026-07-20 17:13:25 +0200

    Add shared user model and update program model

--- lib/models/program.dart
+++ lib/models/program.dart
@@ -1,22 +1,40 @@
 class Program {
+  final int id;
   final String title;
   final String startDate;
   final String endDate;
   final String location;
+  final String category;
   final String description;
   final List<String> requirements;
   final List<String> skills;
   final String image;
 
   Program({
+    required this.id,
     required this.title,
     required this.startDate,
     required this.endDate,
     required this.location,
+    required this.category,
     required this.description,
     required this.requirements,
     required this.skills,
     required this.image,
   });
-}
 
+  factory Program.fromJson(Map<String, dynamic> json) {
+    return Program(
+      id: json['id'],
+      title: json['title'],
+      startDate: json['startDate'],
+      endDate: json['endDate'],
+      location: json['location'],
+      category: json['category'],
+      description: json['description'],
+      requirements: List<String>.from(json['requirements']),
+      skills: List<String>.from(json['skills']),
+      image: json['image'],
+    );
+  }
+}
---
+++ lib/models/user.dart
@@ -0,0 +1,25 @@
+class User {
+  final int id;
+  final String name;
+  final String email;
+  final String password;
+  final String role;
+
+  User({
+    required this.id,
+    required this.name,
+    required this.email,
+    required this.password,
+    required this.role,
+  });
+
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(
+      id: json['id'],
+      name: json['name'],
+      email: json['email'],
+      password: json['password'],
+      role: json['role'],
+    );
+  }
+}
